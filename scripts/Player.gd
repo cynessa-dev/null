218625f8e5973c4a914ac19bcb43f8b2efebd80f
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-20 09:29:04 +0800

    ✨ feat: add way for player to interact with NPCs

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,9 +1,14 @@
+# Player.gd
 extends CharacterBody2D
 
 const SPEED := 120.0
 
+# This will get populated when player gets near NPCs
+var nearby_npc: Node = null
+
 func _physics_process(delta: float) -> void:
 	_handle_movement()
+	_handle_interaction()
 	move_and_slide()
 
 func _handle_movement() -> void:
@@ -12,3 +17,13 @@ func _handle_movement() -> void:
 		Input.get_axis("ui_up", "ui_down")
 	).normalized()
 	velocity = direction * SPEED
+
+func _handle_interaction() -> void:
+	if Input.is_action_just_pressed("interact") and nearby_npc != null:
+		nearby_npc.interact()
+
+func set_nearby_npc(npc: Node) -> void:
+	nearby_npc = npc
+
+func clear_nearby_npc() -> void:
+	nearby_npc = null
